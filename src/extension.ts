fa81b9fdbcc0ffcdb399cad24ce0388b0217a05d
Bronley Plumb <bronley@gmail.com>
2017-10-25 15:56:40 -0400

    Added support for formatting document. It also honors the vscode-provided indent style.

--- src/extension.ts
+++ src/extension.ts
@@ -3,23 +3,41 @@ import { BrightScriptFormatter } from 'brightscript-formatter';
 
 export function activate(context: vscode.ExtensionContext) {
     vscode.languages.registerDocumentFormattingEditProvider({ language: 'brightscript', scheme: 'file' }, {
-        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+        provideDocumentFormattingEdits(document: vscode.TextDocument, options) {
+            let lineEnding = document.eol === vscode.EndOfLine.CRLF ? '\r\n' : '\n';
             try {
-                var lastLine = document.lineAt(document.lineCount - 1);
-                var range = new vscode.Range(
-                    new vscode.Position(0, 0),
-                    new vscode.Position(document.lineCount, lastLine.text.length)
-                );
-                var text = document.getText(range);
+                var text = document.getText();
                 let formatter = new BrightScriptFormatter
-                var formattedText = formatter.format(text);
-    
-                let edit = new vscode.TextEdit(range, formattedText);
-                return [edit];
+                var formattedText = formatter.format(text, {
+                    indentSpaceCount: options.tabSize,
+                    indentStyle: options.insertSpaces ? 'spaces' : 'tabs'
+                });
+
+                let edits = getEditChunks(formattedText);
+
+                return edits;
             } catch (e) {
                 vscode.window.showErrorMessage(e.message, e.stack.split('\n')[0]);
             }
 
+            function getEditChunks(formattedText: string) {
+                let lines = formattedText.split(lineEnding);
+                //make an edit per line of the doc
+                let edits: vscode.TextEdit[] = [];
+                for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
+                    let formattedLine = lines[lineNumber];
+
+                    let docLine = document.lineAt(lineNumber);
+                    var range = new vscode.Range(
+                        new vscode.Position(lineNumber, 0),
+                        new vscode.Position(lineNumber, docLine.text.length)
+                    );
+                    let validatedRange = document.validateRange(range);
+                    let edit = vscode.TextEdit.replace(range, formattedLine);
+                    edits.push(edit);
+                }
+                return edits;
+            }
         }
     });
 }
