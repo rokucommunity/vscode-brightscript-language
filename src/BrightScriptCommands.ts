70f4fafa8f54445fcb7c31ff7bc8fe776d4e787b
Blake Visin <bvisin@beachbody.com>
2018-12-14 21:58:36 -0800

    Added configuration for host and sendRemoteCommand function

--- src/BrightScriptCommands.ts
+++ src/BrightScriptCommands.ts
@@ -1,4 +1,8 @@
 import * as vscode from 'vscode';
+import * as request from 'request';
+
+// tslint:disable-next-line
+export var __request: any = request;
 
 import BrightScriptFileUtils from './BrightScriptFileUtils';
 
@@ -15,12 +19,24 @@ export default class BrightScriptCommands {
     }
 
     private fileUtils: BrightScriptFileUtils;
+    private context: vscode.ExtensionContext;
     public function;
 
-    public registerCommands({ subscriptions }: vscode.ExtensionContext) {
+    public rokuDeploy = require('roku-deploy');
+    private valueName = 'isInRemoteMode';
+
+    public registerCommands(context: vscode.ExtensionContext) {
+        this.context = context;
+        let subscriptions = context.subscriptions;
         subscriptions.push(vscode.commands.registerCommand('extension.brightscript.toggleXML', () => {
             this.onToggleXml();
         } ));
+        subscriptions.push(vscode.commands.registerCommand('extension.brightscript.sendRemoteCommand', (key: string) => {
+            this.sendRemoteCommand(key);
+        } ));
+        subscriptions.push(vscode.commands.registerCommand('extension.brightscript.toggleRemote', async () => {
+            await this.onToggleRemoteMode();
+        }));
     }
 
     public async openFile(filename: string) {
@@ -36,6 +52,32 @@ export default class BrightScriptCommands {
             if (alternateFileName) {
                 this.openFile(alternateFileName);
             }
+        }
+    }
+
+    public async onToggleRemoteMode() {
+        let isInRemoteMode = this.context.workspaceState.get(this.valueName, false);
+        console.log(`onToggleRemoteMode ${this.valueName} was ${isInRemoteMode}. Setting it to ${!isInRemoteMode}`);
+        await this.context.workspaceState.update(this.valueName, !isInRemoteMode);
+    }
+
+    public async sendRemoteCommand(key){
+        let isInRemoteMode = this.context.workspaceState.get(this.valueName, false);
+        console.log(`sendHome ${this.valueName} is ${isInRemoteMode}`);
+        if (isInRemoteMode){
+            let config = vscode.workspace.getConfiguration('brightscript.remoteControl', null);
+            let host = config.get("host")
+            let clickUrl = `http://${host}:8060/keypress/${key}`;
+            console.log(`send ${clickUrl}`);
+            return new Promise(function (resolve, reject) {
+                request.post(clickUrl, function (err, response) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(response);
+                });
+            });
+
         }
     }
 }
