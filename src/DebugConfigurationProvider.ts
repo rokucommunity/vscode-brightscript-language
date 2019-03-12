0539949fe80057533c1aacdd37640121d7a59955
Bronley Plumb <bronley@gmail.com>
2019-03-11 21:37:38 -0400

    Support launch configurations that exist in user settings. Fixes #111

--- src/DebugConfigurationProvider.ts
+++ src/DebugConfigurationProvider.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import * as fsExtra from 'fs-extra';
+import * as path from 'path';
 import { FilesType } from 'roku-deploy';
 import {
     CancellationToken,
@@ -31,9 +32,29 @@ export class BrightScriptDebugConfigurationProvider implements DebugConfiguratio
     public async resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: BrightScriptDebugConfiguration, token?: CancellationToken): Promise<DebugConfiguration> {
         //make sure we have an object
         config = config ? config : {} as any;
+        let folderUri: vscode.Uri;
+        //us ethe workspace folder provided
+        if (folder) {
+            folderUri = folder.uri;
+
+            //if there's only one workspace, use that workspace's folder path
+        } else if (vscode.workspace.workspaceFolders.length === 1) {
+            folderUri = vscode.workspace.workspaceFolders[0].uri;
+        } else {
+            //there are multiple workspaces, ask the user to specify which one they want to use
+            let workspaceFolder = await vscode.window.showWorkspaceFolderPick();
+            if (workspaceFolder) {
+                folderUri = workspaceFolder.uri;
+            }
+        }
+
+        if (!folderUri) {
+            //cancel this whole thing because we can't continue without the user specifying a workspace folder
+            throw new Error('Cannot determine which workspace to use for brightscript debugging');
+        }
 
         //load the brsconfig settings (if available)
-        let brsconfig = await util.getBrsConfig(folder.uri);
+        let brsconfig = await util.getBrsConfig(folderUri);
         if (brsconfig) {
             config = Object.assign({}, brsconfig, config);
         }
@@ -51,6 +72,16 @@ export class BrightScriptDebugConfigurationProvider implements DebugConfiguratio
         config.retainStagingFolder = config.retainStagingFolder === true ? true : false;
         config.clearOutputOnLaunch = config.clearOutputOnLaunch === true ? true : false;
         config.selectOutputOnLogMessage = config.selectOutputOnLogMessage === true ? true : false;
+
+        //for rootDir, replace workspaceFolder now to avoid issues in vscode itself
+        if (config.rootDir.indexOf('${workspaceFolder}') > -1) {
+            config.rootDir = path.normalize(config.rootDir.replace('${workspaceFolder}', folderUri.fsPath));
+        }
+
+        //for outDir, replace workspaceFolder now
+        if (config.outDir.indexOf('${workspaceFolder}') > -1) {
+            config.outDir = path.normalize(config.outDir.replace('${workspaceFolder}', folderUri.fsPath));
+        }
 
         //prompt for host if not hardcoded
         if (config.host.trim() === '${promptForHost}') {
@@ -76,7 +107,7 @@ export class BrightScriptDebugConfigurationProvider implements DebugConfiguratio
             let envFilePath = config.envFile;
             //resolve ${workspaceFolder} so we can actually load the .env file now
             if (config.envFile.indexOf('${workspaceFolder}') > -1) {
-                envFilePath = config.envFile.replace('${workspaceFolder}', folder.uri.fsPath);
+                envFilePath = config.envFile.replace('${workspaceFolder}', folderUri.fsPath);
             }
             if (await this.util.fileExists(envFilePath) === false) {
                 throw new Error(`Cannot find .env file at "${envFilePath}`);
