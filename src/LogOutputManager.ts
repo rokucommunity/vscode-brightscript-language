19ed47bcb6fb534f57b06e008f7bc3a78d3404a0
George Cook <george.cook@twintechs.com>
2019-03-26 13:58:11 -0500

    variable name fix

--- src/LogOutputManager.ts
+++ src/LogOutputManager.ts
@@ -158,7 +158,7 @@ export class LogOutputManager {
         let lines = lineText.split('\n');
         lines.forEach((line) => {
             if (line !== '') {
-                if (!(this.config.output || {}).includeStackTracesInLogOutput) {
+                if (!(this.config.output || {}).includeStackTraces) {
                         // filter out debugger noise
                     if (line.match(this.debugStartRegex)) {
                         console.log('start MicroDebugger block');
