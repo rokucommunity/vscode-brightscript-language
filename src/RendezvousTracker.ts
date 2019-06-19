634569bb31dec0197a7780461c6840160ab88c07
Christopher Dwyer-Perkins <chris.dwyer-perkins@redspace.com>
2019-06-19 13:17:06 -0300

    Updated the rendezvous tracking regex to better handle file paths that do not start with pgk and unblocks that have no impact cost.

--- src/RendezvousTracker.ts
+++ src/RendezvousTracker.ts
@@ -8,7 +8,7 @@ export class RendezvousTracker {
 
     public processLogLine(logLine: string): boolean {
         let match;
-        if (match = /(\[sg\.node\.(BLOCK|UNBLOCK)\] Rendezvous\[(\d+)\])[\s\w]+(?:(pkg:\/[\w\/]+\.brs)\((\d+)\)|(\d+\.\d+))/g.exec(logLine)) {
+        if (match = /(\[sg\.node\.(BLOCK|UNBLOCK)\] Rendezvous\[(\d+)\])[\s\w]+(?:[?<=\s](\w+:\/[\w\/]+\.brs)\((\d+)\)|(\d+\.\d+)|\n)/g.exec(logLine)) {
             let newMatch = match;
             return true;
         }
