2c39f8b2cbc268d9205216553e2282bc6258d0ea
Christopher Dwyer-Perkins <chris.dwyer-perkins@redspace.com>
2019-06-21 11:05:24 -0300

    Updated the logic in the RendezvousTracker to store the rendezvous data and track the number of hits and total time for each.

--- src/RendezvousTracker.ts
+++ src/RendezvousTracker.ts
@@ -2,9 +2,11 @@
 export class RendezvousTracker {
     constructor() {
         this.rendezvousHistory = {};
+        this.rendezvousBlocks = {};
     }
 
-    private rendezvousHistory: object;
+    private rendezvousHistory: RendezvousHistory;
+    private rendezvousBlocks: RendezvousBlocks;
 
     public processLogLine(logLine: string): string {
         let lines = logLine.split('\n');
@@ -14,7 +16,36 @@ export class RendezvousTracker {
         lines.map((line) => {
             let match;
             if (match = /\[sg\.node\.(BLOCK|UNBLOCK)\] Rendezvous\[(\d+)\](?:\s\w+\n|\s\w{2}\s(.*brs)\((\d+)\)|[\s\w]+(\d+\.\d+)+|\s\w+)/g.exec(line)) {
-                let newMatch = match;
+                let [fullMatch, type, id, fileName, lineNumber, duration] = match;
+                if (type === 'BLOCK') {
+                    this.rendezvousBlocks[id] = {
+                        fileName: fileName,
+                        lineNumber: lineNumber
+                    };
+                } else if (type === 'UNBLOCK' && this.rendezvousBlocks[id]) {
+                    let blockInfo = this.rendezvousBlocks[id];
+
+                    if (this.rendezvousHistory[blockInfo.fileName]) {
+                        if (this.rendezvousHistory[blockInfo.fileName][blockInfo.lineNumber]) {
+                            this.rendezvousHistory[blockInfo.fileName][blockInfo.lineNumber].totalTime += this.getTime(duration);
+                            this.rendezvousHistory[blockInfo.fileName][blockInfo.lineNumber].hitCount ++;
+                        } else {
+                            this.rendezvousHistory[blockInfo.fileName][blockInfo.lineNumber] = {
+                                totalTime: this.getTime(duration),
+                                hitCount: 1
+                            };
+                        }
+                    } else {
+                        this.rendezvousHistory[blockInfo.fileName] = {
+                            [blockInfo.lineNumber]: {
+                                totalTime: this.getTime(duration),
+                                hitCount: 1
+                            }
+                        };
+                    }
+
+                    delete this.rendezvousBlocks[id];
+                }
             } else if (line) {
                 normalOutput += line + '\n';
             }
@@ -22,4 +53,24 @@ export class RendezvousTracker {
 
         return normalOutput;
     }
+
+    private getTime(duration?: string): number {
+        return duration ? parseFloat(duration) : 0.000;
+    }
+}
+
+export interface RendezvousHistory {
+    [key: string]: {
+        [key: string]: {
+            totalTime: number;
+            hitCount: number;
+        }
+    };
+}
+
+interface RendezvousBlocks {
+    [key: string]: {
+        fileName: string;
+        lineNumber: string;
+    };
 }
