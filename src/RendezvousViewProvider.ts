2d4c907cc992a61438e9963573f344d73e2fc0d1
Christopher Dwyer-Perkins <chris.dwyer-perkins@redspace.com>
2019-07-04 12:06:34 -0300

    Cleaned up the sorting commands and logic.

--- src/RendezvousViewProvider.ts
+++ src/RendezvousViewProvider.ts
@@ -10,29 +10,24 @@ export class RendezvousViewProvider implements vscode.TreeDataProvider<vscode.Tr
 
     constructor(context: vscode.ExtensionContext) {
         this.tree = {};
-
-        this.sortReverse = true;
-        this.activeFilter = [
-            'details.totalTime',
-            rendezvousAverageTimeSort,
-            rendezvousHitCountSort,
-            compare('label'),
-            { reverse: this.shouldSortBeReverse }
-        ];
+        this.enableSmartSorting();
 
         // #region Register sorting commands
         let subscriptions = context.subscriptions;
-        subscriptions.push(vscode.commands.registerCommand('extension.brightscript.sortRendezvousByFileName', () => {
+        subscriptions.push(vscode.commands.registerCommand('extension.brightscript.toggleSortingMethod', () => {
+            if (!this.enableSmartSorting()) {
+                this.disableSmartSorting();
+            }
             this._onDidChangeTreeData.fire();
         }));
 
-        subscriptions.push(vscode.commands.registerCommand('extension.brightscript.clearRendezvousSorting', () => {
-            this.activeFileFilter = noSort;
+        subscriptions.push(vscode.commands.registerCommand('extension.brightscript.toggleSortingAscDesc', () => {
+            this.sortAscending = !this.sortAscending;
             this._onDidChangeTreeData.fire();
         }));
+        // #endregion
 
         vscode.commands.registerCommand('RendezvousViewProvider.openFile', (resource) => this.openResource(resource));
-        // #endregion
     }
 
     // tslint:disable-next-line:variable-name
@@ -41,10 +36,30 @@ export class RendezvousViewProvider implements vscode.TreeDataProvider<vscode.Tr
 
     private tree: RendezvousHistory;
     private activeFilter: any;
-    private sortReverse: boolean;
+    private sortAscending: boolean = false;
+    private isUsingSmartSorting: boolean;
 
-    get shouldSortBeReverse(): boolean {
-        return this.sortReverse;
+    private enableSmartSorting(): boolean {
+        if (!this.isUsingSmartSorting) {
+            this.isUsingSmartSorting = true;
+            this.activeFilter = [
+                this.rendezvousTotalTimeSort,
+                this.rendezvousAverageTimeSort,
+                this.rendezvousHitCountSort,
+                this.compare('label')
+            ];
+            return true;
+        }
+        return false;
+    }
+
+    private disableSmartSorting() {
+        if (this.isUsingSmartSorting) {
+            this.isUsingSmartSorting = false;
+            this.activeFilter = [this.compare('label')];
+            return true;
+        }
+        return false;
     }
 
     public onDidReceiveDebugSessionCustomEvent(e: any) {
@@ -61,12 +76,14 @@ export class RendezvousViewProvider implements vscode.TreeDataProvider<vscode.Tr
 
     public getChildren(element: RendezvousTreeItem): RendezvousTreeItem[] {
         if (!element) {
+            // There are no tree view items so we should be creating file tree items
             return arraySort(Object.keys(this.tree).map((key) => {
                 if (!isRendezvousDetailsField(key)) {
                     return new RendezvousFileTreeItem(key, vscode.TreeItemCollapsibleState.Collapsed, null, this.tree[key]);
                 }
             }), this.activeFilter);
         } else {
+            // VS code is asking for the children of the supplied tree item
             let treeElement = this.getTreeElement(element);
 
             let result;
@@ -74,7 +91,7 @@ export class RendezvousViewProvider implements vscode.TreeDataProvider<vscode.Tr
                 result = arraySort(Object.keys(treeElement).map((key) => {
                     if (!isRendezvousDetailsField(key) && treeElement[key].totalTime > 0) {
                         let { hitCount, totalTime, clientPath, clientLineNumber } = treeElement[key];
-                        let label = `line: (${key}) | hitCount: ${hitCount} | totalTime: ${totalTime.toFixed(3)} s | average: ${(totalTime / hitCount).toFixed(3) } s`;
+                        let label = `line: ${key} | hitCount: ${hitCount} | totalTime: ${totalTime.toFixed(3)} s | average: ${(totalTime / hitCount).toFixed(3) } s`;
                         let command = { command: 'RendezvousViewProvider.openFile', title: 'Open File', arguments: [{ path: clientPath, lineNumber: clientLineNumber }], };
 
                         return new RendezvousTreeItem(label, vscode.TreeItemCollapsibleState.None, element, key, treeElement[key], command);
@@ -89,7 +106,7 @@ export class RendezvousViewProvider implements vscode.TreeDataProvider<vscode.Tr
         return element;
     }
 
-    public getTreeElement(element: RendezvousTreeItem): {[key: string]: any} {
+    private getTreeElement(element: RendezvousTreeItem): {[key: string]: any} {
         let objectPath = [];
         let currentObject: {[key: string]: any} = this.tree;
 
@@ -115,6 +132,54 @@ export class RendezvousViewProvider implements vscode.TreeDataProvider<vscode.Tr
         let doc = await vscode.workspace.openTextDocument(uri);
         let range = new Range(new Position(fileArgs.lineNumber - 1, 0), new Position(fileArgs.lineNumber - 1, 0));
         await vscode.window.showTextDocument(doc, { preview: false, selection: range });
+    }
+
+    private rendezvousTotalTimeSort: IRendezvousItemSort = (itemOne: RendezvousTreeItem, itemTwo: RendezvousTreeItem) => {
+        if (itemOne.details.totalTime > itemTwo.details.totalTime) {
+            return this.handleReverseSort(-1);
+        } else if (itemOne.details.totalTime < itemTwo.details.totalTime) {
+            return this.handleReverseSort(1);
+        }
+        return 0;
+    }
+
+    private rendezvousAverageTimeSort: IRendezvousItemSort = (itemOne: RendezvousTreeItem, itemTwo: RendezvousTreeItem) => {
+        let zeroCostOffsetOne = itemOne.details.zeroCostHitCount ? itemOne.details.zeroCostHitCount : 0;
+        let zeroCostOffsetTwo = itemTwo.details.zeroCostHitCount ? itemTwo.details.zeroCostHitCount : 0;
+
+        if (itemOne.details.totalTime / (itemOne.details.hitCount - zeroCostOffsetOne) > itemTwo.details.totalTime / (itemTwo.details.hitCount - zeroCostOffsetTwo)) {
+            return this.handleReverseSort(-1);
+        } else if (itemOne.details.totalTime / (itemOne.details.hitCount - zeroCostOffsetOne) < itemTwo.details.totalTime / (itemTwo.details.hitCount - zeroCostOffsetTwo)) {
+            return this.handleReverseSort(1);
+        }
+        return 0;
+    }
+
+    private rendezvousHitCountSort: IRendezvousItemSort = (itemOne: RendezvousTreeItem, itemTwo: RendezvousTreeItem) => {
+        let zeroCostOffsetOne = itemOne.details.zeroCostHitCount ? itemOne.details.zeroCostHitCount : 0;
+        let zeroCostOffsetTwo = itemTwo.details.zeroCostHitCount ? itemTwo.details.zeroCostHitCount : 0;
+
+        if ((itemOne.details.hitCount - zeroCostOffsetOne) > (itemTwo.details.hitCount - zeroCostOffsetTwo)) {
+            return this.handleReverseSort(-1);
+        } else if ((itemOne.details.hitCount - zeroCostOffsetOne) < (itemTwo.details.hitCount - zeroCostOffsetTwo)) {
+            return this.handleReverseSort(1);
+        }
+        return 0;
+    }
+
+    private compare = (propertyName): IRendezvousItemSort => {
+        return (itemOne, itemTwo): number => {
+            if (itemOne[propertyName] > itemTwo[propertyName]) {
+                return this.handleReverseSort(-1);
+            } else if (itemOne[propertyName] < itemTwo[propertyName]) {
+                return this.handleReverseSort(1);
+            }
+            return 0;
+        };
+    }
+
+    private handleReverseSort(sortResult: number): number {
+        return (!this.sortAscending) ? sortResult : (sortResult === 1) ? -1 : (sortResult === -1) ? 1 : sortResult;
     }
 
     private objectDiff(obj1, obj2, exclude?) {
@@ -169,17 +234,6 @@ export class RendezvousTreeItem extends vscode.TreeItem {
     get tooltip(): string {
         return `${this.label}`;
     }
-
-    // get description(): string {
-    //     return this.version;
-    // }
-
-    // public iconPath = {
-    //     light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
-    //     dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
-    // };
-
-    // public contextValue = 'dependency';
 }
 
 export class RendezvousFileTreeItem extends vscode.TreeItem {
@@ -202,40 +256,3 @@ export class RendezvousFileTreeItem extends vscode.TreeItem {
 }
 
 type IRendezvousItemSort = (itemOne: RendezvousTreeItem, itemTwo: RendezvousTreeItem) => number;
-
-const noSort: IRendezvousItemSort = (itemOne: RendezvousTreeItem, itemTwo: RendezvousTreeItem) => {
-    return 0;
-};
-
-const rendezvousAverageTimeSort: IRendezvousItemSort = (itemOne: RendezvousTreeItem, itemTwo: RendezvousTreeItem) => {
-    let zeroCostOffsetOne = itemOne.details.zeroCostHitCount ? itemOne.details.zeroCostHitCount : 0;
-    let zeroCostOffsetTwo = itemTwo.details.zeroCostHitCount ? itemTwo.details.zeroCostHitCount : 0;
-
-    if (itemOne.details.totalTime / (itemOne.details.hitCount - zeroCostOffsetOne)  > itemTwo.details.totalTime / (itemTwo.details.hitCount - zeroCostOffsetTwo)) {
-        return -1;
-    } else if (itemOne.details.totalTime / (itemOne.details.hitCount - zeroCostOffsetOne) < itemTwo.details.totalTime / (itemTwo.details.hitCount - zeroCostOffsetTwo)) {
-        return 1;
-    } else {
-        return 0;
-    }
-};
-
-const rendezvousHitCountSort: IRendezvousItemSort = (itemOne: RendezvousTreeItem, itemTwo: RendezvousTreeItem) => {
-    let zeroCostOffsetOne = itemOne.details.zeroCostHitCount ? itemOne.details.zeroCostHitCount : 0;
-    let zeroCostOffsetTwo = itemTwo.details.zeroCostHitCount ? itemTwo.details.zeroCostHitCount : 0;
-
-    if ((itemOne.details.hitCount - zeroCostOffsetOne) > (itemTwo.details.hitCount - zeroCostOffsetTwo)) {
-        return -1;
-    } else if ((itemOne.details.hitCount - zeroCostOffsetOne) < (itemTwo.details.hitCount - zeroCostOffsetTwo)) {
-        return 1;
-    } else {
-        return 0;
-    }
-};
-
-// reusable compare function
-function compare(prop) {
-    return function(a, b) {
-        return a[prop].localeCompare(b[prop]);
-    };
-}
