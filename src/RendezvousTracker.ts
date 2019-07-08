bfbf53dd6cddc69e698994fabca2ad33ecaac29c
Christopher Dwyer-Perkins <chris.dwyer-perkins@redspace.com>
2019-07-08 10:32:28 -0300

    Updated the regex in the RendezvousTracker to take into account the newly descovered output format.

--- src/RendezvousTracker.ts
+++ src/RendezvousTracker.ts
@@ -41,8 +41,8 @@ export class RendezvousTracker {
 
         lines.map((line) => {
             let match;
-            // see the following for an explanation for this regex: https://regex101.com/r/In0t7d/4
-            if (match = /\[sg\.node\.(BLOCK|UNBLOCK)\] Rendezvous\[(\d+)\](?:\s\w+\n|\s\w{2}\s(.*brs|.*xml)\((\d+)\)|[\s\w]+(\d+\.\d+)+|\s\w+)/g.exec(line)) {
+            // see the following for an explanation for this regex: https://regex101.com/r/In0t7d/5
+            if (match = /\[sg\.node\.(BLOCK|UNBLOCK)\s{0,}\] Rendezvous\[(\d+)\](?:\s\w+\n|\s\w{2}\s(.*)\((\d+)\)|[\s\w]+(\d+\.\d+)+|\s\w+)/g.exec(line)) {
                 let [fullMatch, type, id, fileName, lineNumber, duration] = match;
                 if (type === 'BLOCK') {
                     this.rendezvousBlocks[id] = {
