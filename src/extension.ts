27c61c9cad221a576f8b0739ed1a6e7f49e7b099
Bronley Plumb <bronley@gmail.com>
2019-09-18 05:07:18 -0400

    Registered the code formatter for brighterscript files.

--- src/extension.ts
+++ src/extension.ts
@@ -54,6 +54,10 @@ export function activate(context: vscode.ExtensionContext) {
         language: 'brightscript',
         scheme: 'file'
     }, new Formatter());
+    vscode.languages.registerDocumentRangeFormattingEditProvider({
+        language: 'brighterscript',
+        scheme: 'file'
+    }, new Formatter());
     outputChannel = vscode.window.createOutputChannel('BrightScript Log');
 
     let configProvider = new BrsDebugConfigurationProvider(context, activeDeviceManager);
