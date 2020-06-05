18dc93c269353a365391da7bf1f818cff7b04ffc
George Cook <george@tantawowa.com>
2020-06-05 12:39:14 -0500

    fixes file utils method

--- src/BrightScriptFileUtils.ts
+++ src/BrightScriptFileUtils.ts
@@ -1,15 +1,23 @@
 export default class BrightScriptFileUtils {
+    /**
+     * Utility method to simplify getting alternate xml/brs|bs extensions
+     * @param  {string} fileName
+     * @returns {string} Returns the xml file name, if filename is a brs or bs file, otherwise the brs filename.
+     */
     public getAlternateFileName(fileName: string): string | undefined {
-        if (fileName !== undefined && (fileName.toLowerCase().endsWith('.brs'))) {
-            return fileName.substring(0, fileName.length - 4) + '.xml';
-        } else if (fileName !== undefined && (fileName.toLowerCase().endsWith('.bs'))) {
-            return fileName.substring(0, fileName.length - 3) + '.xml';
-        } else if (fileName !== undefined && fileName.toLowerCase().endsWith('.xml')) {
+        let lowerFileName = fileName?.toLowerCase();
+        if (lowerFileName.endsWith('.brs') || lowerFileName?.endsWith('.bs')) {
+            return fileName.replace(/\.b(r?)s$/, '.xml');
+        } else if (lowerFileName?.endsWith('.xml')) {
             return fileName.substring(0, fileName.length - 4) + '.brs';
-        } else {
-            return undefined;
         }
     }
+
+    /**
+     * Utility method to simplify getting alternate brs/bs extensions
+     * @param  {string} fileName
+     * @returns {string} Returns the brs file name, if filename is a brs file, otherwise the bs filename. If filenAMe is neither, then fileName is returned unchanged
+     */
     public getAlternateBrsFileName(fileName: string): string | undefined {
         if (fileName !== undefined && (fileName.toLowerCase().endsWith('.bs'))) {
             return fileName.substring(0, fileName.length - 2) + 'brs';
@@ -20,21 +28,4 @@ export default class BrightScriptFileUtils {
         }
     }
 
-    public getBrsFileName(fileName: string): string | undefined {
-        if (fileName !== undefined && (fileName.toLowerCase().endsWith('.bs'))) {
-            return fileName.substring(0, fileName.length - 2) + 'brs';
-        } else if (fileName.toLowerCase().endsWith('.brs')) {
-            return fileName;
-        }
-    }
-
-    public getBsFileName(fileName: string): string | undefined {
-        if (fileName !== undefined && (fileName.toLowerCase().endsWith('.brs'))) {
-            return fileName.substring(0, fileName.length - 3) + 'bs';
-        } else if (fileName.toLowerCase().endsWith('.bs')) {
-            return fileName;
-        } else {
-            return undefined;
-        }
-    }
 }
