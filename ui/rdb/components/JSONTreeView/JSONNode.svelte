fab96bf8736880c73ac1a727071e28d7979b642b
Brian Leighty <bleighty@fubo.tv>
2021-07-21 11:31:07 -0400

    Combine JSONNested and JSONObjectNode into JSONNode to get rid of circular reference

--- ui/rdb/components/JSONTreeView/JSONNode.svelte
+++ ui/rdb/components/JSONTreeView/JSONNode.svelte
@@ -1,14 +1,54 @@
 <script>
-    import JSONObjectNode from "./JSONObjectNode.svelte";
-    import JsonValueNode from "./JSONValueNode.svelte";
+    import JSONKey from './JSONKey.svelte';
+    import JSONArrow from './JSONArrow.svelte';
+
     export let nodeValue, nodeKey;
-    let valueType = typeof nodeValue;
     export let onValueChange = () => {};
 
-    let componentType = JSONObjectNode;
-    if (valueType !== "object") {
-        componentType = JsonValueNode;
+    let expanded = false;
+
+    function toggleExpand() {
+        expanded = !expanded;
+    }
+    function expand() {
+        expanded = true;
     }
 </script>
 
-<svelte:component bind:nodeValue={nodeValue} onValueChange={onValueChange} this={componentType} {nodeKey} />
+<style>
+    li {
+        user-select: text;
+        word-wrap: break-word;
+        word-break: break-all;
+    }
+
+    .valueNode {
+        color: #ececec;
+    }
+
+    ul {
+        margin: 0;
+        list-style: none;
+        padding-left: 1.2rem;
+        user-select: none;
+    }
+</style>
+
+{#if typeof nodeValue !== 'object'}
+    <li>
+        <JSONKey key={nodeKey} />
+        <label for="jsonNodeValue" on:blur={onValueChange} bind:textContent={nodeValue} class="valueNode" contenteditable="true" />
+    </li>
+{:else}
+    <li>
+        <JSONArrow {expanded} on:click={toggleExpand} />
+        <JSONKey key={nodeKey}/>
+        {#if expanded}
+            <ul on:click={expand}>
+                {#each Object.keys(nodeValue) as key}
+                    <svelte:self onValueChange={onValueChange} nodeKey={key} bind:nodeValue={nodeValue[key]} />
+                {/each}
+            </ul>
+        {/if}
+    </li>
+{/if}
--- ui/rdb/components/JSONTreeView/JSONTreeView.svelte
+++ ui/rdb/components/JSONTreeView/JSONTreeView.svelte
@@ -37,9 +37,9 @@
 </style>
 
 <ul>
-    {#each keys as key, index}
+    {#each keys as key}
         <JSONNode
-            onValueChange={(e) => onValueChange(key)}
+            onValueChange={() => onValueChange(key)}
             nodeKey={key}
             isParentExpanded={false}
             nodeValue={registryValues[key]} />
