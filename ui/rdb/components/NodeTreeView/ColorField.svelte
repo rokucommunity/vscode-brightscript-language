aad690568cd6e67b97a87a357107459f3754e180
Brian Leighty <bleighty@tubi.tv>
2022-05-02 16:23:17 -0400

    Add tests for ColorField

--- ui/rdb/components/NodeTreeView/ColorField.svelte
+++ ui/rdb/components/NodeTreeView/ColorField.svelte
@@ -3,12 +3,13 @@
 <script lang="ts">
     import hexRgb from 'hex-rgb';
     import { createEventDispatcher } from 'svelte';
+    import { colorField } from "./ColorField";
     const dispatch = createEventDispatcher();
 
     export let id: string; id; // Used to silence the warning that we're not using it
     export let integerColor: number;
     $: {
-        hexColor = convertRgbToHex(convertIntegerColorToRgb(integerColor));
+        hexColor = colorField.convertRgbToHex(colorField.convertIntegerColorToRgb(integerColor));
     }
 
     export let value = '';
@@ -19,33 +20,14 @@
             const rgb = hexRgb(hexColor)
             textColor = rgb.red * 0.299 + rgb.green * 0.587 + rgb.blue * 0.114 * rgb.alpha > 186 ? '#000000' : '#FFFFFF';
             value = hexColor;
+            // Triggers update to be sent to parent's on:input
             dispatch('input');
         } catch {}
     }
 
     let textColor: string;
 
-    function convertIntegerColorToRgb(integerColor: number) {
-        // Have to convert from signed to unsigned and then convert to binary representation
-        const binary = (integerColor >>> 0).toString(2).padStart(32, '0');
 
-        // Slice out each 8 bits for each rgba part value
-        const rgb = {
-            red: parseInt(binary.slice(0, 8), 2),
-            green: parseInt(binary.slice(8, 16), 2),
-            blue: parseInt(binary.slice(16, 24), 2),
-            alpha: parseInt(binary.slice(24, 32), 2),
-        }
-        return rgb
-    }
-
-    function convertHexPart(byte: number) {
-        return byte.toString(16).padStart(2, '0').toUpperCase();
-    }
-
-    function convertRgbToHex(rgb: ReturnType<typeof convertIntegerColorToRgb>) {
-        return `#${convertHexPart(rgb.red)}${convertHexPart(rgb.green)}${convertHexPart(rgb.blue)}${convertHexPart(rgb.alpha)}`;
-    }
 </script>
 <style>
     div {
