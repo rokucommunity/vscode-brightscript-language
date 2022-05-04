122af651be6bf8800c8c4c33d21d0afad0c60c73
Brian Leighty <bleighty@tubi.tv>
2022-05-04 12:20:33 -0400

    Add comment about making sure to set rdb comment after screen show

--- ui/rdb/components/Common/ODCSetupSteps.svelte
+++ ui/rdb/components/Common/ODCSetupSteps.svelte
@@ -12,7 +12,7 @@
 <div id="odcSetupSteps">
     This tool requires a component be included and initialized in your application (similar to RALE). The easiest way to do this is:
     <ul>
-        <li>Include the following comment in either your Scene or main.brs file<br><span class="codeSnippet">' vscode_rdb_on_device_component_entry</span></li>
+        <li>Include the following comment in either your Scene or main.brs file (if including in main.brs, be sure to add after your roSGScreen screen.show() call)<br><span class="codeSnippet">' vscode_rdb_on_device_component_entry</span><br></li>
         <li>Make sure your launch.json configuration has<br><span class="codeSnippet">"injectRdbOnDeviceComponent": true</span> included in it</li>
     </ul>
     The extension can copy the files automatically for you so there's no need to handle that part. If you are still having issues even with these steps, check to make sure you're seeing this line in your device logs <span class="codeSnippet">[RTA][INFO] OnDeviceComponent init</span>
