337ef7ce198a753f39627c8e98689af72f9e4a59
Brian Leighty <bleighty@tubi.tv>
2022-07-07 20:57:36 -0400

    Add toggling node visibility, moving node position and view of node fields

--- ui/rdb/components/SceneGraphInspectorPanel/NodeBranchView.svelte
+++ ui/rdb/components/SceneGraphInspectorPanel/NodeBranchView.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
     import type { ODC } from 'roku-test-automation';
+    import throttle from 'just-throttle';
+    import { odc } from '../../ExtensionIntermediary';
     import { utils } from '../../utils';
+    import Loader from '../../components/Common/Loader.svelte';
     import { createEventDispatcher } from 'svelte';
-    import Edit from 'svelte-codicons/lib/Edit.svelte';
+    import { Edit, Eye, EyeClosed, DebugBreakpointDataUnverified, Move } from 'svelte-codicons';
     import Chevron from '../Common/Chevron.svelte';
-    import DebugBreakpointDataUnverified from 'svelte-codicons/lib/DebugBreakpointDataUnverified.svelte';
     const dispatch = createEventDispatcher();
 
     export let nodeTree: ODC.NodeTree;
     export let depth = 0;
     let self: HTMLDivElement;
+    let nodeInfoElement: HTMLDivElement;
+    let nodeFieldsCache: ODC.NodeRepresentation?;
 
     const expandedStorageKey = `expanded:${nodeTree.ref}`;
     export let expanded = utils.getStorageBooleanValue(expandedStorageKey);
@@ -51,9 +55,124 @@
         dispatch('open', nodeTree);
     }
 
+    let nodeInfoHideTimeout: ReturnType<typeof setTimeout>;
+    function startNodeInfoTimeout() {
+        stopNodeInfoTimeout();
+        nodeInfoHideTimeout = setTimeout(async () => {
+            nodeInfoElement.classList.add('hide');
+        }, 100);
+    }
+
+    function stopNodeInfoTimeout() {
+        clearTimeout(nodeInfoHideTimeout);
+    }
+
+    function onEditButtonMouseEnter() {
+        stopNodeInfoTimeout()
+        nodeInfoElement.classList.remove('hide');
+    }
+
+    function onEditButtonMouseLeave() {
+        // When the mouse leaves the edit button start timeout
+        startNodeInfoTimeout();
+    }
+
+    function onNodeInfoMouseEnter() {
+        // Prevent the node info from hiding while mouse it over itself
+        stopNodeInfoTimeout();
+    }
+
+    function onNodeInfoMouseLeave() {
+        // When the mouse leaves the node info start timeout
+        startNodeInfoTimeout();
+    }
+
+    let requestTimeout: ReturnType<typeof setTimeout>;
+    function onNodeMouseEnter() {
+        if (!nodeFieldsCache) {
+             // Delay timer to avoid trying to load data for every single item the user scrolls by
+            requestTimeout = setTimeout(async () => {
+                let args: ODC.GetValueAtKeyPathArgs = {
+                    base: 'nodeRef',
+                    keyPath: `${nodeTree.ref}`
+                }
+                if (nodeTree.subtype === 'Global') {
+                    args = {
+                        base: 'global',
+                        keyPath: ''
+                    }
+                }
+                const {value} = await odc.getValueAtKeyPath(args);
+                nodeFieldsCache = value;
+            }, 60);
+        }
+    }
+
+    function onNodeMouseLeave() {
+        clearTimeout(requestTimeout);
+    }
+
+    let lastXScreenPosition;
+    let lastYScreenPosition;
+    function onMoveNodePositionDown(e) {
+        this.setPointerCapture(e.pointerId);
+
+        lastXScreenPosition = e.x;
+        lastYScreenPosition = e.y;
+    }
+
+    async function onMoveNodePosition(e: PointerEvent) {
+        // Only do something if the mouse is actually clicked
+        if (e.buttons < 1) {
+            return;
+        }
+
+        const translation = nodeFieldsCache.translation;
+        translation[0] += Math.floor(e.x - lastXScreenPosition);
+        translation[1] += Math.floor(e.y - lastYScreenPosition);
+
+        lastXScreenPosition = e.x;
+        lastYScreenPosition = e.y;
+
+        await odc.setValueAtKeyPath({
+            base: 'nodeRef',
+            keyPath: `${nodeTree.ref}.translation`,
+            value: translation
+        });
+    }
+
+    async function toggleNodeVisiblity() {
+        await odc.setValueAtKeyPath({
+            base: 'nodeRef',
+            keyPath: `${nodeTree.ref}.visible`,
+            value: !nodeFieldsCache.visible
+        });
+        nodeFieldsCache.visible = !nodeFieldsCache.visible;
+    }
+
     function onChildExpanded() {
         expanded = true;
         dispatch('childExpanded');
+    }
+
+    function buildFieldHoverText(field) {
+        if (field === null) {
+            return 'invalid';
+        } else if (Array.isArray(field)) {
+            let result = [];
+            for (const item of field) {
+                result.push(buildFieldHoverText(item));
+            }
+            return '[' + result.join(', ') + ']';
+        } else if (typeof field === 'object') {
+            let result = [];
+            for (const key in field) {
+                result.push(`${key}: ${buildFieldHoverText(field[key])}`);
+            }
+            return '{' + result.join(', ') + '}';
+        } else {
+            return field;
+        }
     }
 </script>
 
@@ -77,12 +196,17 @@
         background-color: var(--vscode-list-hoverBackground);
     }
 
+    .hide {
+        display: none;
+    }
+
     .children {
         position: relative;
     }
 
     .self:hover .actions {
         display: flex;
+        background-color: var(--vscode-sideBar-background);
     }
 
     .actions {
@@ -111,9 +235,32 @@
         border-left: 1px solid var(--vscode-tree-indentGuidesStroke);
         opacity: 0.4;
     }
+
     .content {
         display: flex;
         padding: 4px 0px;
+    }
+
+    .nodeInfo {
+        background-color: var(--vscode-editorHoverWidget-background);
+        color: var(--vscode-editorHoverWidget-foreground);
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: 100%;
+        min-height: 70px;
+        z-index: 100;
+        border-bottom: 4px solid var(--vscode-tab-border);
+    }
+
+    .nodeInfo ul {
+        margin: 0;
+        padding: 5px 15px;
+        overflow-wrap: break-word;
+    }
+
+    ul li {
+        list-style: none;
     }
 </style>
 
@@ -121,6 +268,8 @@
     class="self"
     bind:this={self}
     class:selected
+    on:mouseenter="{onNodeMouseEnter}"
+    on:mouseleave="{onNodeMouseLeave}"
     on:click|stopPropagation={toggleExpand}>
     {#each { length: depth ?? 0 } as _, i}
         <span class="indent-guide">&nbsp;</span>
@@ -134,14 +283,50 @@
             {/if}
         </span>
         <span class="nodeName">
-            {nodeTree.subtype}
+            {nodeTree.subtype}{#if nodeTree.id.length > 0}&nbsp;id: {nodeTree.id}{/if}
         </span>
-        {#if nodeTree.id.length > 0}&nbsp;id: {nodeTree.id}{/if}
     </div>
     <div class="actions">
-        <span title="Edit" class="icon-button" on:click={open}>
+        {#if nodeFieldsCache}
+            {#if nodeFieldsCache.translation !== undefined}
+                <span
+                    title="Move Node Position"
+                    class="icon-button"
+                    on:click|stopPropagation
+                    on:pointerdown={onMoveNodePositionDown}
+                    on:pointermove={throttle(onMoveNodePosition, 33)}>
+                    <Move />
+                </span>
+            {/if}
+            {#if nodeFieldsCache.visible === true || nodeFieldsCache.visible === false}
+                <span title="{nodeFieldsCache.visible ? 'Hide Node' : 'Show Node'}" class="icon-button" on:click|stopPropagation={toggleNodeVisiblity}>
+                    {#if nodeFieldsCache.visible}<Eye />{:else}<EyeClosed />{/if}
+                </span>
+            {/if}
+        {/if}
+        <span
+            title="Edit Node"
+            class="icon-button editButton"
+            on:click|stopPropagation={open}
+            on:mouseenter={onEditButtonMouseEnter}
+            on:mouseleave={onEditButtonMouseLeave}>
             <Edit />
         </span>
+    </div>
+    <div
+        class="nodeInfo hide"
+        on:mouseenter={onNodeInfoMouseEnter}
+        on:mouseleave={onNodeInfoMouseLeave}
+        bind:this={nodeInfoElement}>
+        <ul>
+            {#if nodeFieldsCache}
+                {#each Object.keys(nodeFieldsCache) as key}
+                    <li>{key}: {buildFieldHoverText(nodeFieldsCache[key])}</li>
+                {/each}
+            {:else}
+                <Loader />
+            {/if}
+        </ul>
     </div>
 </div>
 <div class="children" class:hide={!expanded}>
