f9192e3521b743eaa9b8f37031841999c439a89e
philanderson888 <philanderson888@hotmail.com>
2022-09-19 22:28:00 +0100

    add eslint improvements to file

--- scripts/sync-launch-settings.ts
+++ scripts/sync-launch-settings.ts
@@ -1,24 +1,24 @@
-console.log('Sync settings from launch to debug`')
-console.log('===================================')
-const packageJsonFile = '../package.json'
+console.log('Sync settings from launch to debug`');
+console.log('===================================');
+const packageJsonFile = '../package.json';
 const packageJson = require(packageJsonFile);
-const { EOL } = require('os')
+const { EOL } = require('os');
 const fs = require('fs');
 
 // extract object from 'package.json'
 const configurationArray = packageJson.contributes.configuration;
 
-const newDebugProperties = {}
+const newDebugProperties = {};
 let debugIndex;
 
-for (let index = 0; index < configurationArray.length; index++){
-    const configurationItem = configurationArray[index]
-    if (configurationItem.id === "debug"){
-        debugIndex = index
-        const debugProperties = configurationItem.properties
-        for (const [key, data] of Object.entries(debugProperties)){
+for (let index = 0; index < configurationArray.length; index++) {
+    const configurationItem = configurationArray[index];
+    if (configurationItem.id === 'debug') {
+        debugIndex = index;
+        const debugProperties = configurationItem.properties;
+        for (const [key, data] of Object.entries(debugProperties)) {
             // start off by just copying over the existing values (straight 1-1 copy)
-            newDebugProperties[key] = data
+            newDebugProperties[key] = data;
         }
     }
 }
@@ -26,32 +26,29 @@ for (let index = 0; index < configurationArray.length; index++){
 // now have to also join in data from package.json .... other part of file ...
 const launchProperties = packageJson.contributes.debuggers[0].configurationAttributes.launch.properties;
 
-for (const [key,data] of Object.entries(launchProperties)){
-    console.log(`${key} has values ..`)
-    const launchProperty = {}
-    for (const [dataKey, dataValue] of Object.entries(data)){
-        console.log(`\t${dataKey} : ${dataValue}`)
-        launchProperty[dataKey] = dataValue
+for (const [key, data] of Object.entries(launchProperties)) {
+    const launchProperty = {};
+    for (const [dataKey, dataValue] of Object.entries(data)) {
+        launchProperty[dataKey] = dataValue;
     }
-    const brightscriptDebugKey = 'brightscript.debug.' + key
+    const brightscriptDebugKey = 'brightscript.debug.' + key;
     // note: if duplicate keys exist, we overwrite (new source data should be identical anyway)
-    newDebugProperties[brightscriptDebugKey] = launchProperty
+    newDebugProperties[brightscriptDebugKey] = launchProperty;
 }
 
 // have to parse final output object to get clean JSON
 const newDebugPropertiesParsed = JSON.parse(JSON.stringify(newDebugProperties))
 
 // finally replace elements back into 'package.json' file
-configurationArray[debugIndex].properties = newDebugPropertiesParsed
-packageJson.contributes.configuration = configurationArray
+configurationArray[debugIndex].properties = newDebugPropertiesParsed;
+packageJson.contributes.configuration = configurationArray;
 
 try {
-    fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 4), 'utf8')
-    fs.appendFileSync(packageJsonFile, EOL, 'utf8')
-}
-catch(e){
-    console.error(e)
+    fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 4), 'utf8');
+    fs.appendFileSync(packageJsonFile, EOL, 'utf8');
+} catch (e) {
+    console.error(e);
 }
 
-console.log('launch settings synchronised with debug settings inside package.json file')
-console.log(packageJson.contributes.configuration)
+console.log('launch settings synchronised with debug settings inside package.json file');
+console.log(packageJson.contributes.configuration);
