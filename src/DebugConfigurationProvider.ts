9284742d6bff73d2b72cffbbc38a8beca7c57b4d
Christian Holbrook <cholbrook@fubo.tv>
2023-01-18 14:17:41 -0700

    Add launch configuration options to disable hiding prefixed variables

--- src/DebugConfigurationProvider.ts
+++ src/DebugConfigurationProvider.ts
@@ -43,6 +43,7 @@ export class BrightScriptDebugConfigurationProvider implements DebugConfiguratio
             disableScreenSaver: true,
             retainStagingFolder: false,
             enableVariablesPanel: true,
+            hidePrefixVariables: true,
             enableDebuggerAutoRecovery: false,
             stopDebuggerOnAppExit: false,
             autoRunSgDebugCommands: [],
@@ -224,6 +225,7 @@ export class BrightScriptDebugConfigurationProvider implements DebugConfiguratio
         config.disableScreenSaver = config.disableScreenSaver === false ? false : this.configDefaults.disableScreenSaver;
         config.retainStagingFolder ??= this.configDefaults.retainStagingFolder;
         config.enableVariablesPanel = 'enableVariablesPanel' in config ? config.enableVariablesPanel : this.configDefaults.enableVariablesPanel;
+        config.hidePrefixVariables = config.hidePrefixVariables === false ? false : this.configDefaults.hidePrefixVariables;
         config.enableDebuggerAutoRecovery = config.enableDebuggerAutoRecovery === true ? true : this.configDefaults.enableDebuggerAutoRecovery;
         config.stopDebuggerOnAppExit = config.stopDebuggerOnAppExit === true ? true : this.configDefaults.stopDebuggerOnAppExit;
         config.files = config.files ? config.files : this.configDefaults.files;
