859e2c8c9fa1231a6d591a75d91bf6162cea65f6
Bronley Plumb <bronley@gmail.com>
2023-10-11 09:23:20 -0400

    Handle enum mismatch for diagnostic severity

--- src/managers/DiagnosticManager.ts
+++ src/managers/DiagnosticManager.ts
@@ -20,11 +20,13 @@ export class DiagnosticManager {
         if (documentUri !== undefined) {
             let result: vscode.Diagnostic[] = [];
             for (const diagnostic of diagnostics) {
+
                 result.push({
                     code: diagnostic.code,
-                    message: `From debugger: ${diagnostic.message}`,
+                    message: diagnostic.message,
                     source: diagnostic.source,
-                    severity: diagnostic.severity as vscode.DiagnosticSeverity,
+                    //the DiagnosticSeverity.Error from vscode-languageserver-types starts at 1, but vscode.DiagnosticSeverity.Error starts at 0. So subtract 1 to make them compatible
+                    severity: diagnostic.severity - 1,
                     tags: diagnostic.tags,
                     range: new vscode.Range(
                         new vscode.Position(diagnostic.range.start.line, diagnostic.range.start.character),
