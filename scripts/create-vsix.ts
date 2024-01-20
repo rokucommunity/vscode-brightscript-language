317fe4f7676decebddaf77dab419a4d21984eef0
Bronley Plumb <bronley@gmail.com>
2024-01-19 22:08:38 -0500

    Overhaul the script

--- scripts/create-vsix.ts
+++ scripts/create-vsix.ts
@@ -1,93 +1,139 @@
-const fsExtra = require('fs-extra');
-const path = require('path');
-const childProcess = require('child_process');
-const chalk = require('chalk');
+import * as fsExtra from 'fs-extra';
+import * as path from 'path';
+import * as childProcess from 'child_process';
+import * as chalk from 'chalk';
 
-const tmpPath = `${__dirname}/../.vsix-building`;
+const silent = process.argv.includes('--silent');
+const tempDir = s`${__dirname}/../.vsix-building`;
 const baseUrl = 'https://github.com/rokucommunity';
+const projects = [{
+    name: 'roku-deploy',
+    dependencies: []
+}, {
+    name: 'brighterscript',
+    dependencies: ['roku-deploy']
+}, {
+    name: 'roku-debug',
+    dependencies: ['roku-deploy', 'brighterscript']
+}, {
+    name: 'brighterscript-formatter',
+    dependencies: ['brighterscript']
+}, {
+    name: 'vscode-brightscript-language',
+    dependencies: ['brighterscript', 'roku-debug', 'brighterscript-formatter', 'roku-deploy']
+}] as Project[];
+
 function main() {
     //create a temp directory for this process
-    log(`Creating and cleaning ${tmpPath}`);
-    fsExtra.emptyDirSync(tmpPath);
-    process.chdir(tmpPath);
+    log(`Creating and cleaning ${tempDir}`);
+    fsExtra.emptyDirSync(tempDir);
+    process.chdir(tempDir);
 
     const branch = process.argv[2].replace(/^refs\/heads\//, '');
 
-    //clone the extension repo
-    clone(
-        'vscode-brightscript-language',
-        hasBranch('vscode-brightscript-language', branch) ? branch : 'master'
-    );
-    const buildVersion = `9001.0.0-${branch.replace(/[^a-zA-Z0-9]/g, '-')}.${Date.now()}`;
-
-    //bump the version for this build
-    changeVersion('vscode-brightscript-language', buildVersion);
-
-    console.log('Installing node_modules for vscode-brightscript-language');
-    execSync('npm install', { cwd: 'vscode-brightscript-language' });
-
-    //clone and build all the repositories
-    for (const project of ['roku-deploy', 'roku-debug', 'brighterscript', 'brighterscript-formatter']) {
-        //only clone projects that have the same brach name as our target
-        if (hasBranch(project, branch)) {
-            log(`Installing local version of ${project}`);
-
-            clone(project, branch);
-            changeVersion(project, buildVersion);
-            execSync(`npm i && npm run build && npm pack`, {
-                cwd: project
-            });
-            //install this package into the extension
-            execSync(`npm i file:/../${project}/${project}-${buildVersion}.tgz`, { cwd: 'vscode-brightscript-language' });
-        } else {
-            log(`Using ${project} from npm`);
-        }
+    //build and link all the projects
+    for (const project of projects) {
+        processProject(project, branch);
     }
 
     log('Building and packaging the extension');
-    execSync("npm i && npm run build && npm run package", { cwd: 'vscode-brightscript-language' });
+    execSync('npm run package', { cwd: 'vscode-brightscript-language' });
+}
+
+function processProject(project: Project, branch: string) {
+    //if this project has already been processed, skip
+    if (project.processed) {
+        log(`${project.name}: already processed`);
+        return;
+    }
+    log(`${project.name}: processing`);
+    const projectBranch = hasBranch(project, branch) ? branch : 'master';
+    const buildVersion = `9001.0.0-${projectBranch.replace(/[^a-zA-Z0-9]/g, '-')}.${Date.now()}`;
+
+    clone(project, projectBranch);
+    changeVersion(project, buildVersion);
+    execSync(`npm i`, {
+        cwd: project.name
+    });
+    for (const dependencyName of project.dependencies) {
+        log(`${project.name}: Processing dependency '${dependencyName}'`);
+        const dependency = projects.find(x => x.name === dependencyName)!;
+        processProject(dependency, branch);
+        //install the dependency into this project
+        execSync(`npm i ${dependency.packagePath}`, { cwd: project.name });
+    }
+    execSync(`npm i && npm run build && npm pack`, {
+        cwd: project.name
+    });
+
+    project.packagePath = `file:/${tempDir}/${project.name}/${project.name}-${buildVersion}.tgz`;
+    project.processed = true;
+    log(`${project.name}: done`);
+}
+
+interface Project {
+    name: string;
+    dependencies: string[];
+    packagePath?: string;
+    processed: boolean;
 }
 
 /**
  * Determine if a repo has a branch with the given name
  */
-function hasBranch(project, branch) {
-    const output = childProcess.execSync(`git ls-remote --heads ${baseUrl}/${project}`).toString();
+function hasBranch(project: Project, branch: string) {
+    const output = childProcess.execSync(`git ls-remote --heads ${baseUrl}/${project.name}`).toString();
     const regexp = new RegExp(`refs/heads/${escapeRegExp(branch)}\\b`);
     return !!regexp.exec(output);
 }
 
-function escapeRegExp(string) {
+function escapeRegExp(string: string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
 
-function clone(project, branch) {
-    const url = `${baseUrl}/${project}`;
+function clone(project: Project, branch: string) {
+    const url = `${baseUrl}/${project.name}`;
     log(`Cloning ${url}`);
-    execSync(`git clone ${url} ${project}`);
+    execSync(`git clone ${url} ${project.name}`);
     execSync(`git checkout ${branch}`, {
-        cwd: `./${project}`
+        cwd: project.name
     });
 }
 
-function changeVersion(project, version) {
-    const packageJson = fsExtra.readJsonSync(`${project}/package.json`);
+function changeVersion(project: Project, version: string) {
+    const packageJson = fsExtra.readJsonSync(`${project.name}/package.json`);
     packageJson.version = version;
-    fsExtra.writeJsonSync(`${project}/package.json`, packageJson);
+    fsExtra.writeJsonSync(`${project.name}/package.json`, packageJson, { spaces: 4 });
 
-    const packageLockJson = fsExtra.readJsonSync(`${project}/package-lock.json`);
+    const packageLockJson = fsExtra.readJsonSync(`${project.name}/package-lock.json`);
     packageLockJson.version = version;
-    fsExtra.writeJsonSync(`${project}/package-lock.json`, packageLockJson);
+    fsExtra.writeJsonSync(`${project.name}/package-lock.json`, packageLockJson, { spaces: 4 });
 }
 
-function execSync(command, options) {
-    options = Object.assign({ stdio: 'inherit' }, options);
+function execSync(command: string, options?: childProcess.ExecSyncOptions) {
+    options = { stdio: 'inherit', ...options };
+    if (silent) {
+        delete options.stdio;
+    }
     log(command + ' ' + JSON.stringify(options));
     return childProcess.execSync(command, options);
 }
 
-function log(message) {
+function log(message: string) {
     console.log(`\n${chalk.blueBright(message)}\n`);
+}
+
+export function s(stringParts, ...expressions: any[]) {
+    let result: string[] = [];
+    for (let i = 0; i < stringParts.length; i++) {
+        result.push(stringParts[i], expressions[i]);
+    }
+
+    return path.resolve(
+        path.normalize(
+            result.join('')
+        )
+    ).replace(/[\/\\]+/g, '/');
 }
 
 main();
