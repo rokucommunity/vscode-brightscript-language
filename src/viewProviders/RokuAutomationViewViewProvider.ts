630b37bcbe69ac8e19e832dbb7341ae44e3437d5
justin tucker <jtuckermail@gmail.com>
2024-10-29 17:57:03 -0700

    ensure selected run is preserved between sessions

--- src/viewProviders/RokuAutomationViewViewProvider.ts
+++ src/viewProviders/RokuAutomationViewViewProvider.ts
@@ -17,6 +17,7 @@ export class RokuAutomationViewViewProvider extends BaseRdbViewProvider {
         super(context, dependencies);
 
         this.addMessageCommandCallback(ViewProviderCommand.storeRokuAutomationConfigs, async (message) => {
+            this.selectedConfig = message.context.selectedConfig;
             this.rokuAutomationConfigs = message.context.configs;
             // Make sure to use JSON.stringify or weird stuff happens
             await context.workspaceState.update(WorkspaceStateKey.rokuAutomationConfigs, JSON.stringify(message.context));
@@ -87,6 +88,7 @@ export class RokuAutomationViewViewProvider extends BaseRdbViewProvider {
     }
 
     private isRecording = false;
+    private selectedConfig;
     private rokuAutomationConfigs: {
         name: string;
         steps: {
@@ -153,10 +155,12 @@ export class RokuAutomationViewViewProvider extends BaseRdbViewProvider {
         const json = this.extensionContext.workspaceState.get(WorkspaceStateKey.rokuAutomationConfigs);
         if (typeof json === 'string') {
             const result = JSON.parse(json);
+            this.selectedConfig = result.selectedConfig;
             this.rokuAutomationConfigs = result.configs;
         }
 
         const message = this.createEventMessage(ViewProviderEvent.onRokuAutomationConfigsLoaded, {
+            selectedConfig: this.selectedConfig,
             configs: this.rokuAutomationConfigs
         });
 
