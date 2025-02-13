75bac91ccc7024a55c3c4b7c283dbedd27e84d7d
Bronley Plumb <bronley@gmail.com>
2025-02-13 16:23:38 -0500

    Make the layout more responsive.

--- webviews/src/views/RokuAppOverlaysView/RokuAppOverlaysView.svelte
+++ webviews/src/views/RokuAppOverlaysView/RokuAppOverlaysView.svelte
@@ -228,83 +228,121 @@
 </script>
 
 <style>
-    #container {
-        padding: 8px;
-    }
-
-    table {
-        border-spacing: 0;
-        width: 100%;
-    }
-
-    .innerBox {
-        table-layout: fixed;
-        margin: 3px;
-    }
-
-    .srcPathLabel {
-        word-wrap: break-word;
+    .container {
+        width:100%;
+        box-sizing: border-box;
         overflow: hidden;
-        max-height: 50px;
-        font-size: 75%;
+        display: grid;
+        grid-template-columns: 0.4fr 0.4fr 3.2fr 0.1fr 0.5fr;
+        grid-template-rows: 1fr 1fr;
+        gap: 0px 0px;
+        min-width: 0;
+        min-height: 0;
+        grid-auto-flow: row;
+        grid-template-areas:
+            'checkbox image filepath filepath delete'
+            'checkbox image label slider delete';
     }
 
-    img:hover {
-        cursor: pointer;
+    .filepath {
+        grid-area: filepath;
+        justify-self: left;
+        align-self: center;
+        overflow: hidden;
+        min-width: 0;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        width: 100%;
+        direction: rtl;
+        text-align: left;
+        opacity: .5;
+    }
+
+    .checkbox {
+        grid-area: checkbox;
+        justify-self: center;
+        align-self: center;
+    }
+
+    .image {
+        grid-area: image;
+        justify-self: center;
+        align-self: center;
+        align-content: center;
+        width: 50px;
+        height: 50px;
+        margin: 5px;
+        text-align: center;
+        border: 1px solid var(--vscode-scrollbarSlider-background);
+        border-radius: 5px;
+    }
+    .image img {
+        max-width: 50px;
+        height: auto;
+        max-height: 50px;
+        margin: auto;
+    }
+
+    .label {
+        grid-area: label;
+        justify-self: left;
+        align-self: center;
+        width:100%;
+    }
+    .label vscode-text-field {
+        width:100%;
+    }
+
+    .slider {
+        grid-area: slider;
+        justify-self: center;
+        align-self: center;
+    }
+    .slider-input{
+        width: 75px;
+    }
+
+    .delete {
+        grid-area: delete;
+        justify-self: center;
+        align-self: center;
     }
 </style>
 
 
-<div id="container">
-    {#if odcAvailable}
-        {#if overlays.length }
-            <table>
-                {#each overlays as overlay, index}
-                    <tr>
-                        <td>
-                            <vscode-checkbox id="{index}" on:change={onOverlayVisibleChange} checked={overlay.visible} />
-                        </td>
-                        <td>
-                            <vscode-button id="{index}" appearance="icon" title="Open Overlay File" aria-label="Open Overlay File" on:click={onOpenFile}>
-                                <img src="{overlay.imageData}" data-file="{overlay.sourcePath}" style="width:32px; min-width:32px; height:32px" />
-                            </vscode-button>
-                        </td>
-                        <td>
-                            <vscode-text-field id="{index}" on:input={onOverlayNameChange} value="{overlay.name}" />
-                        </td>
-                        <td>
-                            <table class="innerBox">
-                                <tr>
-                                    <td>
-                                        <div class="srcPathLabel" title="{overlay.sourcePath}">{overlay.sourcePath}</div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input id="{index.toString()}" type="range" min="0" max="100" value="{overlay.opacity * 100}" class="slider" on:input={onOverlayOpacityChange}>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                        <td>
-                            <vscode-button id="{index}" appearance="icon" title="Delete Overlay" aria-label="Delete Overlay" on:click={deleteOverlay}>
-                                <Trash />
-                            </vscode-button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="5">
-                            <vscode-divider />
-                        </td>
-                    </tr>
-                {/each}
-            </table>
-        {:else}
-            <span style="padding:10px">
-                You haven't added any overlays. Add one by clicking <NewFile />
-            </span>
-        {/if}
+{#if odcAvailable}
+    {#if overlays.length }
+        {#each overlays as overlay, index}
+            <div class="container">
+                <div class="filepath" title="{overlay.sourcePath}">
+                    {overlay.sourcePath}
+                </div>
+                <div class="checkbox">
+                    <vscode-checkbox id="{index}" on:change={onOverlayVisibleChange} checked={overlay.visible} />
+                </div>
+                <div class="image">
+                    <img src="{overlay.imageData}" data-file="{overlay.sourcePath}"/>
+                </div>
+                <div class="label">
+                    <vscode-text-field id="{index}" on:input={onOverlayNameChange} value="{overlay.name}" />
+                </div>
+                <div class="slider">
+                    <input id="{index.toString()}" class="slider-input" type="range" min="0" max="100" value="{overlay.opacity * 100}" on:input={onOverlayOpacityChange}>
+                </div>
+                <div class="delete">
+                    <vscode-button id="{index}" appearance="icon" title="Delete Overlay" aria-label="Delete Overlay" on:click={deleteOverlay}>
+                        <Trash />
+                    </vscode-button>
+                </div>
+            </div>
+                <vscode-divider />
+        {/each}
     {:else}
-        <OdcSetupSteps />
+        <span style="padding:10px">
+            You haven't added any overlays. Add one by clicking <NewFile />
+        </span>
     {/if}
-</div>
+{:else}
+    <OdcSetupSteps />
+{/if}
