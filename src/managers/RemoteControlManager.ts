c805a13bb72bf172904a4f04ad4cf0b56710d59f
Christopher Dwyer-Perkins <chris@inverted-solutions.com>
2025-03-26 12:16:00 -0300

    More refinement

--- src/managers/RemoteControlManager.ts
+++ src/managers/RemoteControlManager.ts
@@ -54,19 +54,16 @@ export class RemoteControlManager {
         const currentMode = vscodeContextManager.get<boolean>('brightscript.isRemoteControlMode', false);
         await this.setRemoteControlMode(!currentMode, initiator);
 
-        //remove focus from statusbar item after clicking
-        if (initiator === 'statusbar') {
-            //focus the active text editor (if there is one)
-            if (vscode.window.activeTextEditor?.document) {
-                await vscode.window.showTextDocument(vscode.window.activeTextEditor.document);
+        //focus the active text editor (if there is one)
+        if (vscode.window.activeTextEditor?.document) {
+            await vscode.window.showTextDocument(vscode.window.activeTextEditor.document);
 
-                //there's no active text editor. Move focus away from the statusbar item (somehow)
-            } else {
-                //focus the next editor group, then the previous editor group.
-                //This is safe to call when there are no editor groups, so it's a good way to remove focus from the statusbar item
-                await vscode.commands.executeCommand('workbench.action.focusNextGroup');
-                await vscode.commands.executeCommand('workbench.action.focusPreviousGroup');
-            }
+            //there's no active text editor. Move focus away from the the currently focused view item (somehow)
+        } else {
+            //focus the next editor group, then the previous editor group.
+            //This is safe to call when there are no editor groups, so it's a good way to remove focus from the statusbar item
+            await vscode.commands.executeCommand('workbench.action.focusNextGroup');
+            await vscode.commands.executeCommand('workbench.action.focusPreviousGroup');
         }
     }
 
