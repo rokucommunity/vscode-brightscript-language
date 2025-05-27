eb0cd847ad40293d2a039593f8e9bf097c3ee1af
Brian Leighty <bleighty@tubi.tv>
2025-05-27 17:38:01 -0400

    Fix: Update JSON parsing in formatValues to not handle strings that look like numbers and booleans to avoid registry updates failing when sent to the device

--- webviews/src/views/RokuRegistryView/RokuRegistryView.spec.ts
+++ webviews/src/views/RokuRegistryView/RokuRegistryView.spec.ts
@@ -4,31 +4,45 @@ import { registryView } from './RokuRegistryView';
 describe('RegistryView', () => {
     describe('formatValues', () => {
         it('should convert json into an object with the correct values', () => {
+            const inputValue = 1;
             const input = {
-                a: '{"b": 1}'
+                a: `{"b": ${inputValue}}`
             };
             const result = registryView.formatValues(input);
-            expect(result.a.b).to.equal(1);
+            expect(result.a.b).to.equal(inputValue);
         });
 
         it('should correctly handle multiple levels', () => {
+            const inputValue = 1;
             const input = {
                 a: {
-                    b: '{"c": 1}'
+                    b: `{"c": ${inputValue}}`
                 }
             };
             const result = registryView.formatValues(input);
-            expect(result.a.b.c).to.equal(1);
+            expect(result.a.b.c).to.equal(inputValue);
         });
 
-        it('should return the incoming value if not JSON', () => {
+        it('should not try and convert strings that look like numbers into numbers', () => {
+            const inputValue = '1';
             const input = {
                 a: {
-                    b: 1
+                    b: inputValue
                 }
             };
             const result = registryView.formatValues(input);
-            expect(result.a.b).to.equal(input.a.b);
+            expect(result.a.b).to.equal(inputValue);
+        });
+
+        it('should not try and convert strings that look like booleans into booleans', () => {
+            const inputValue = 'true';
+            const input = {
+                a: {
+                    b: inputValue
+                }
+            };
+            const result = registryView.formatValues(input);
+            expect(result.a.b).to.equal(inputValue);
         });
     });
 });
