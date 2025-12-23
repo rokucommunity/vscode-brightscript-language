509aa458c1326218838a91f7904747fc19b3bb4c
Hasim Karaman <Hasim.Karaman@Siriusxm.com>
2025-12-23 23:30:21 +0000

    Add unit tests

--- src/commands/CaptureScreenshotCommand.spec.ts
+++ src/commands/CaptureScreenshotCommand.spec.ts
@@ -1,6 +1,7 @@
 import { vscode } from '../mockVscode.spec';
 import { createSandbox } from 'sinon';
 import { CaptureScreenshotCommand } from './CaptureScreenshotCommand';
+import { BrightScriptCommands } from '../BrightScriptCommands';
 import * as rokuDeploy from 'roku-deploy';
 import { expect } from 'chai';
 import URI from 'vscode-uri';
@@ -8,26 +9,67 @@ import URI from 'vscode-uri';
 const sinon = createSandbox();
 
 describe('CaptureScreenshotCommand', () => {
+    let brightScriptCommands: BrightScriptCommands;
     let command: CaptureScreenshotCommand;
     let context = vscode.context;
     let workspace = vscode.workspace;
 
     beforeEach(() => {
         command = new CaptureScreenshotCommand();
-        command.register(context, undefined);
+        brightScriptCommands = new BrightScriptCommands({} as any, {} as any, {} as any, {} as any, {} as any, {} as any);
+        command.register(context, brightScriptCommands);
     });
 
     afterEach(() => {
         sinon.restore();
-        workspace._configuration = {};
         workspace.workspaceFolders = [];
+    });
+
+    it('gets remoteHost and remotePassword when hostParam is not provided', async () => {
+        sinon.stub(brightScriptCommands, 'getRemoteHost').resolves('1.1.1.1');
+        sinon.stub(brightScriptCommands, 'getRemotePassword').resolves('password');
+
+        const { host, password } = await command['getHostAndPassword']();
+
+        expect(host).to.eql('1.1.1.1');
+        expect(password).to.eql('password');
+    });
+
+    it('gets remotePassword when hostParam matches remoteHost', async () => {
+        await context.workspaceState.update('remoteHost', '1.1.1.1');
+        await context.workspaceState.update('remotePassword', 'password');
+
+        const { host, password } = await command['getHostAndPassword']('1.1.1.1');
+
+        expect(host).to.eql('1.1.1.1');
+        expect(password).to.eql('password');
+    });
+
+    it('prompts for password when hostParam does not match remoteHost', async () => {
+        sinon.stub(vscode.window, 'showInputBox').resolves('password');
+
+        const { host, password } = await command['getHostAndPassword']('1.1.1.1');
+
+        expect(host).to.eql('1.1.1.1');
+        expect(password).to.eql('password');
+    });
+
+    it('shows error message when captureScreenshot fails', async () => {
+        const stub = sinon.stub(command as any, 'getHostAndPassword').callsFake(() => Promise.resolve({ host: '1.1.1.1', password: 'password' }));
+        sinon.stub(rokuDeploy, 'takeScreenshot').rejects(new Error('Screenshot failed'));
+        const stubError = sinon.stub(vscode.window, 'showErrorMessage');
+
+        await command['captureScreenshot']('1.1.1.1');
+
+        expect(stub.getCall(0).args[0]).to.eql('1.1.1.1');
+        expect(stubError.calledOnce).to.be.true;
     });
 
     it('uses temp dir when screenshotDir is not defined', async () => {
         sinon.stub(command as any, 'getHostAndPassword').callsFake(() => Promise.resolve({ host: '1.1.1.1', password: 'password' }));
         const stub = sinon.stub(rokuDeploy, 'takeScreenshot').returns(Promise.resolve('screenshot.png'));
 
-        await command['captureScreenshot']('1.1.1.1');
+        await command['captureScreenshot']();
 
         expect(stub.getCall(0).args[0]).to.eql({ host: '1.1.1.1', password: 'password' });
     });
@@ -46,7 +88,7 @@ describe('CaptureScreenshotCommand', () => {
             }
         ];
 
-        await command['captureScreenshot']('1.1.1.1');
+        await command['captureScreenshot']();
 
         expect(stub.getCall(0).args[0]).to.eql({ host: '1.1.1.1', password: 'password', outDir: '/workspace/screenshots' });
     });
@@ -65,7 +107,7 @@ describe('CaptureScreenshotCommand', () => {
             }
         ];
 
-        await command['captureScreenshot']('1.1.1.1');
+        await command['captureScreenshot']();
 
         expect(stub.getCall(0).args[0]).to.eql({ host: '1.1.1.1', password: 'password', outDir: '/workspace/screenshots' });
     });
@@ -91,7 +133,7 @@ describe('CaptureScreenshotCommand', () => {
         };
         sinon.stub(vscode.window, 'showWorkspaceFolderPick').resolves(workspaceFolders[1]);
 
-        await command['captureScreenshot']('1.1.1.1');
+        await command['captureScreenshot']();
 
         expect(stub.getCall(0).args[0]).to.eql({ host: '1.1.1.1', password: 'password', outDir: '/workspace2/screenshots' });
     });
@@ -117,7 +159,7 @@ describe('CaptureScreenshotCommand', () => {
         };
         sinon.stub(vscode.window, 'showWorkspaceFolderPick').resolves(workspaceFolders[1]);
 
-        await command['captureScreenshot']('1.1.1.1');
+        await command['captureScreenshot']();
 
         expect(stub.getCall(0).args[0]).to.eql({ host: '1.1.1.1', password: 'password', outDir: '/workspace2/screenshots' });
     });
