71a7271923700ae1d8448083cabd227cbd9c3d77
Hasim Karaman <Hasim.Karaman@Siriusxm.com>
2026-01-15 22:37:21 +0000

    Refactoring

--- src/formatter.ts
+++ src/formatter.ts
@@ -15,29 +15,32 @@ import * as path from 'path';
 
 export class Formatter implements DocumentRangeFormattingEditProvider {
 
-    public async provideDocumentRangeFormattingEdits(document: TextDocument, range: Range, options: vscode.FormattingOptions): Promise<TextEdit[]> {
-
+    private getFormatterOptions(document: TextDocument): { bsfmtOptions: FormattingOptions | null; userSettingsOptions: vscode.WorkspaceConfiguration } {
         //TODO is there anything we can to do to better detect when the same file is used in multiple workspaces?
         //vscode seems to pick the lowest workspace (or perhaps the last workspace?)
         const workspaceFolder = workspace.getWorkspaceFolder(document.uri);
-        let userSettingsOptions = workspace.getConfiguration('brightscript.format');
+        const cwd = workspaceFolder?.uri.fsPath ?? process.cwd();
+        const userSettingsOptions = workspace.getConfiguration('brightscript.format');
         const configFile = userSettingsOptions.get<string>('configFile');
-
-        // Resolve the config file path if specified
         let bsfmtPath: string | undefined;
         if (configFile) {
             // Resolve relative to workspace folder, or process.cwd() if no workspace
             // Note: path.resolve() handles absolute paths correctly (returns them as-is)
-            const basePath = workspaceFolder?.uri.fsPath ?? process.cwd();
-            bsfmtPath = path.resolve(basePath, configFile);
+            bsfmtPath = path.resolve(cwd, configFile);
         }
 
-        let bsfmtOptions = new Runner().getBsfmtOptions({
-            cwd: workspaceFolder?.uri.fsPath ?? process.cwd(),
+        const bsfmtOptions = new Runner().getBsfmtOptions({
+            cwd: cwd,
             //we just want bsfmt options...but files is mandatory. Don't worry, we won't actually use it.
             files: [],
             ...(bsfmtPath && { bsfmtPath: bsfmtPath })
         });
+
+        return { bsfmtOptions: bsfmtOptions, userSettingsOptions: userSettingsOptions };
+    }
+
+    public async provideDocumentRangeFormattingEdits(document: TextDocument, range: Range, options: vscode.FormattingOptions): Promise<TextEdit[]> {
+        const { bsfmtOptions, userSettingsOptions } = this.getFormatterOptions(document);
 
         try {
             let text = document.getText();
@@ -57,6 +60,7 @@ export class Formatter implements DocumentRangeFormattingEditProvider {
             await window.showErrorMessage(e.message, e.stack.split('\n')[0]);
         }
     }
+
     private getEditChunks(document: TextDocument, formattedText: string, range: Range) {
         let lines = formattedText?.split(/\r?\n/g);
         //make an edit per line of the doc
