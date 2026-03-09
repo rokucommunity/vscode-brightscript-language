6ae3c95fa12096725edaa696433a26a37b0937d5
Bronley Plumb <bronley@gmail.com>
2026-03-09 16:34:32 -0400

    Fix lint errors

--- src/util.spec.ts
+++ src/util.spec.ts
@@ -1,8 +1,6 @@
 import * as assert from 'assert';
 import { expect } from 'chai';
 import * as fsExtra from 'fs-extra';
-import * as getPort from 'get-port';
-import * as net from 'net';
 import * as path from 'path';
 import * as sinonActual from 'sinon';
 
